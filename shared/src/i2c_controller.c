dce2c2617314e87656c5af581632071588a2d749
divij <divijsasi@gmail.com>
2024-11-10 18:44:00 -0500

    added read function

--- shared/src/i2c_controller.c
+++ shared/src/i2c_controller.c
@@ -40,8 +40,26 @@ int WriteData(uint8_t recipient, uint8_t *data, size_t length)
         return 0;
 }
 
-
-int ReadData(uint8_t address, uint8_t *packet)
+// returns non-zero if error
+int ReadData(uint8_t address, uint8_t *packet) 
 {
+        mxc_i2c_req_t i2c = {
+                // I2C buffer memory address
+                .i2c = I2C_INTERFACE,
+                // The component id of the recipient
+                .addr = address,
+                // Number of bytes to write to the I2C buffer
+                .tx_len = 0,
+                .tx_buf = 0,
+                // number of bytes to read from the I2C buffer
+                .rx_len = 81,
+                .rx_buf = 0,
+                .restart = 0,
+                .callback = NULL
+        };
+        MXC_I2C_Read(&i2c, (char*) packet, &i2c.rx_len, 1);
+        // https://analogdevicesinc.github.io/msdk/Libraries/PeriphDrivers/Documentation/MAX78000/group__i2c.html
+
         return 0;
 }
+
